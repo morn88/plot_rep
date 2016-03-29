8425b2808c6c9dd027064d1a2f5d7d708d7b89da
m0rn <morn88@gmail.com>
2016-03-29 16:39:53 +0300

    Изменение аннотаций к графику

--- annotations.py
+++ annotations.py
@@ -21,6 +21,7 @@ def bytespdata2num(fmt, encoding='utf-8'):
         return strconverter(s)
     return bytesconverter
 
+
 def graph_data(stock):
     fig = plt.figure()
     ax1 = plt.subplot2grid((1, 1), (0, 0))
@@ -58,20 +59,27 @@ def graph_data(stock):
     ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
     ax1.xaxis.set_major_locator(mticker.MaxNLocator(10))
     ax1.grid(True)
-    ax1.annotate('Bad News!', (date[11], highp[11]),
-                 xytext=(0.8, 0.9), textcoords='axes fraction',
-                 arrowprops = dict(facecolor='grey', color='gray'))
 
-    font_dict = {'family': 'serif',
-                 'color': 'darkred',
-                 'size': 15}
-    ax1.text(date[10], closep[1], 'Text Example', fontdict=font_dict)
+    bbox_props = dict(boxstyle='larrow', fc='w', ec='k', lw=1)
+    ax1.annotate(str(closep[-1]), (date[-1], closep[-1]),
+                 xytext=(date[-1]+4, closep[-1]), bbox=bbox_props)
+
+    # # Annatation example with arrow
+    # ax1.annotate('Bad News!', (date[11], highp[11]),
+    #              xytext=(0.8, 0.9), textcoords='axes fraction',
+    #              arrowprops = dict(facecolor='grey', color='gray'))
+    # # Font dict example
+    # font_dict = {'family': 'serif',
+    #              'color': 'darkred',
+    #              'size': 15}
+    # # Hard coded text
+    # ax1.text(date[10], closep[1], 'Text Example', fontdict=font_dict)
 
     plt.xlabel('Date')
     plt.ylabel('Price')
     plt.title('%s Graph' % stock.upper())
     # plt.legend()
-    plt.subplots_adjust(left=0.09, bottom=0.20, right=0.94, top=0.90, wspace=0.2, hspace=0)
+    plt.subplots_adjust(left=0.10, bottom=0.24, right=0.95, top=0.90, wspace=0.2, hspace=0)
     plt.show()
 
 graph_data('ebay')
