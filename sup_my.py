f5b387297e616160952c6af4078504b42a0ae23a
m0rn <morn88@gmail.com>
2016-03-30 16:46:30 +0300

    update file

--- sup_my.py
+++ sup_my.py
@@ -2,3 +2,16 @@ import random
 import matplotlib.pyplot as plt
 
 fig = plt.figure()
+def creat_plot():
+    xs = []
+    ys = []
+
+    for i in range(10):
+        x = i
+        y = random.randrange(10)
+
+        xs.append(x)
+        xy.append(y)
+    return xs, ys
+
+ax1 = fig.add_subplot(1, 1, 1)
