5abf46db084ea8870d6c821bd3c7924c258e7672
m0rn <morn88@gmail.com>
2016-04-04 16:42:01 +0300

    update sup_my file

--- sup_my.py
+++ sup_my.py
@@ -1,8 +1,13 @@
 import random
 import matplotlib.pyplot as plt
+from matplotlib import style
 
+style.use('fivethirtyeight')
+print(style.available)
 fig = plt.figure()
-def creat_plot():
+
+
+def create_plots():
     xs = []
     ys = []
 
@@ -11,7 +16,24 @@ def creat_plot():
         y = random.randrange(10)
 
         xs.append(x)
-        xy.append(y)
+        ys.append(y)
     return xs, ys
 
-ax1 = fig.add_subplot(1, 1, 1)
+ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=1, colspan=1)
+ax2 = plt.subplot2grid((6, 1), (1, 0), rowspan=4, colspan=1)
+ax3 = plt.subplot2grid((6, 1), (5, 0), rowspan=1, colspan=1)
+
+# ax1 = fig.add_subplot(221)
+# ax2 = fig.add_subplot(222)
+# ax3 = fig.add_subplot(212)
+
+x, y = create_plots()
+ax1.plot(x, y)
+
+x, y = create_plots()
+ax2.plot(x, y)
+
+x, y = create_plots()
+ax3.plot(x, y)
+
+plt.show()
