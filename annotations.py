a83fd40f68d3fd3847ec3033d300fd23f6eb22e0
m0rn <morn88@gmail.com>
2016-04-04 21:08:52 +0300

    update annotations

--- annotations.py
+++ annotations.py
@@ -9,9 +9,17 @@ import urllib
 import datetime as dt
 
 style.use('fivethirtyeight')
-# print(plt.style.available)
+print(plt.style.available)
 
-# print(plt.__file__)
+print(plt.__file__)
+
+MA1 = 10
+MA2 = 30
+
+def moving_average(values, window):
+    weights = np.repeat(1.0, window)/window
+    smas = np.convolve(values, weights, 'valid')
+    return smas
 
 
 def bytespdata2num(fmt, encoding='utf-8'):
@@ -24,9 +32,14 @@ def bytespdata2num(fmt, encoding='utf-8'):
 
 def graph_data(stock):
     fig = plt.figure()
-    ax1 = plt.subplot2grid((1, 1), (0, 0))
+    ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=1, colspan=1)
+    plt.title('%s Graph' % stock.upper())
+    ax2 = plt.subplot2grid((6, 1), (1, 0), rowspan=1, colspan=1)
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    ax3 = plt.subplot2grid((6, 1), (5, 0), rowspan=1, colspan=1)
 
-    stock_price_url = 'http://chartapi.finance.yahoo.com/instrument/1.0/' + stock + '/chartdata;type=quote;range=1m/csv'
+    stock_price_url = 'http://chartapi.finance.yahoo.com/instrument/1.0/' + stock + '/chartdata;type=quote;range=10y/csv'
     source_code = urllib.request.urlopen(stock_price_url).read().decode()
     stock_data = []
     split_source = source_code.split('\n')
@@ -51,17 +64,21 @@ def graph_data(stock):
         ohlc.append(append_me)
         x += 1
 
-    candlestick_ohlc(ax1, ohlc, width=0.4, colorup='#77d879', colordown='#db3f3f')
+    ma1 = moving_average(closep, MA1)
+    ma2 = moving_average(closep, MA2)
+    start = len(date[MA2-1:])
 
-    for label in ax1.xaxis.get_ticklabels():
+    candlestick_ohlc(ax2, ohlc, width=0.4, colorup='#77d879', colordown='#db3f3f')
+
+    for label in ax2.xaxis.get_ticklabels():
         label.set_rotation(45)
 
-    ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-    ax1.xaxis.set_major_locator(mticker.MaxNLocator(10))
-    ax1.grid(True)
+    ax2.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    ax2.xaxis.set_major_locator(mticker.MaxNLocator(10))
+    ax2.grid(True)
 
     bbox_props = dict(boxstyle='larrow', fc='w', ec='k', lw=1)
-    ax1.annotate(str(closep[-1]), (date[-1], closep[-1]),
+    ax2.annotate(str(closep[-1]), (date[-1], closep[-1]),
                  xytext=(date[-1]+4, closep[-1]), bbox=bbox_props)
 
     # # Annatation example with arrow
@@ -74,11 +91,9 @@ def graph_data(stock):
     #              'size': 15}
     # # Hard coded text
     # ax1.text(date[10], closep[1], 'Text Example', fontdict=font_dict)
-
-    plt.xlabel('Date')
-    plt.ylabel('Price')
-    plt.title('%s Graph' % stock.upper())
     # plt.legend()
+    ax3.plot(date[-start:], ma1[-start:])
+    ax3.plot(date[-start:], ma2[-start:])
     plt.subplots_adjust(left=0.10, bottom=0.24, right=0.95, top=0.90, wspace=0.2, hspace=0)
     plt.show()
 
