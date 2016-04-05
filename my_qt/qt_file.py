80f9c93e96c0bf42e9cf36433aac3d0170e4061c
m0rn <morn88@gmail.com>
2016-04-05 16:23:59 +0300

    Добавленно меню- и статусбары

--- my_qt/qt_file.py
+++ my_qt/qt_file.py
@@ -8,13 +8,27 @@ class Window(QtGui.QMainWindow):
         self.setGeometry(50, 50, 500, 300)
         self.setWindowTitle("PyQt tuts!")
         self.setWindowIcon(QtGui.QIcon('pythonlogo.png'))
+
+        extractAction = QtGui.QAction("&GET TO THE CHOPPAH!!!", self)
+        extractAction.setShortcut("Ctrl+Q")
+        extractAction.setStatusTip('Leave The App')
+        extractAction.triggered.connect(self.close_application)
+
+        self.statusBar()
+
+        mainMenu = self.menuBar()
+        fileMenu = mainMenu.addMenu('&File')
+        fileMenu.addAction(extractAction)
+        self.setStatusTip('My Window')
+
         self.home()
 
     def home(self):
         btn = QtGui.QPushButton("Quit", self)
         btn.clicked.connect(self.close_application)
         btn.resize(btn.minimumSizeHint())
-        # btn.move(100, 100)
+        btn.move(100, 100)
+        btn.setStatusTip('My Button')
         self.show()
 
     def close_application(self):
