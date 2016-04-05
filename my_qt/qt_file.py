9ec92b6e27aa2a401a351297feb7d4bd4857b6e8
m0rn <morn88@gmail.com>
2016-04-05 14:14:59 +0300

    add button

--- my_qt/qt_file.py
+++ my_qt/qt_file.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
 
 class Window(QtGui.QMainWindow):
@@ -8,8 +8,21 @@ class Window(QtGui.QMainWindow):
         self.setGeometry(50, 50, 500, 300)
         self.setWindowTitle("PyQt tuts!")
         self.setWindowIcon(QtGui.QIcon('pythonlogo.png'))
+        self.home()
+
+    def home(self):
+        btn = QtGui.QPushButton("Quit", self)
+        btn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        btn.resize(100, 100)
+        btn.move(100, 100)
         self.show()
 
-app = QtGui.QApplication(sys.argv)
-GUI = Window()
-sys.exit(app.exec_())
+def run():
+    app = QtGui.QApplication(sys.argv)
+    GUI = Window()
+    sys.exit(app.exec_())
+
+
+
+
+run()
