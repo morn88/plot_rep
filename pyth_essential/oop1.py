fbafbb888be6fa1093fc7f0b5aa827e13c2ce0dd
m0rn <morn88@gmail.com>
2016-04-11 23:36:41 +0300

    property, setter

--- pyth_essential/oop1.py
+++ pyth_essential/oop1.py
@@ -1,32 +1,22 @@
-class Rectangle:
-    def __init__(self, side_a, side_b):
-        self.side_a = side_a
-        self.side_b = side_b
+class MyObject:
+    def __init__(self):
+        self.__attribute = 0
 
-    def __repr__(self):
-        return 'Rectangle(%.1f, %.1f)' % (self.side_a, self.side_b)
+    @property
+    def attribute(self):
+        return self.__attribute
+
+    @attribute.setter
+    def attribute(self, value):
+        if value < 100:
+            self.__attribute = value
 
 
-class Circle:
-    def __init__(self, radius):
-        self.radius = radius
-
-    def __repr__(self):
-        return 'Circle(%.1f)' % self.radius
-
-    @classmethod
-    def from_rectangle(cls, rectangle):
-        radius = (rectangle.side_a ** 2 + rectangle.side_b ** 2) ** 0.5 / 2
-        return cls(radius)
-
-def main():
-    rectangle = Rectangle(3, 4)
-    print(rectangle)
-
-    first_circle = Circle(1)
-    print(first_circle)
-
-    second_circle = Circle.from_rectangle(rectangle)
-    print(second_circle)
-if __name__ == '__main__':
-    main()
+obj = MyObject()
+print(obj.attribute)
+obj.attribute = 20
+print(obj.attribute)
+obj.attribute = 200
+print(obj.attribute)
+obj.attribute = 30
+print(obj.attribute)
